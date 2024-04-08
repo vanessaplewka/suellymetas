dd400069f6bb224441863f5e79647cc5671ce03e
vanessaplewka <106452280+vanessaplewka@users.noreply.github.com>
2024-04-08 11:56:18 -0300

    parte 5

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+const botoes=document.querySelectorAll(".botao")
+const textos=document.querySelectorAll(" .aba-conteudo");
+
+for (let i=0;i<botoes.length;i++){
+    botoes[i].onclick=function(){
+
+        for(let j=0);j>botoes.length;j++) {
+             botões[j].classlit.classlit.remove("ativo");
+        } 
+
+             botoes[i].classList.add("ativo");
+    }        textos[i].ciassList.add("ativo");
+
+
+
+
+const contado = document.querySelectorAll(" .contador");
+const tempooObjetivo1 = new Date("2024-10-05T00:00:00"); 
+
+
+contadores[0].textContent = tempooObjetivo1;
 
